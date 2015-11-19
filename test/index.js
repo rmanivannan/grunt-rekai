32237df0f8e62482c7e3e637f586756cccda8ebf
Manivannan R <manivannan13tnj@gmail.com>
2015-11-20 00:01:26 +0530

    Update index.js

--- test/index.js
+++ test/index.js
@@ -8,7 +8,6 @@ describe('grunt-rekai', function() {
   });
   it('Should contain correct fingerprint value.', function() {
     var fileCnt = fs.readFileSync(filename,'utf8');
-    console.log(fileCnt);
     assert.ok(fileCnt.indexOf('Ao43b9f1RlduffmZRujo7g==') >= 0);
     //assert.ok(fileCnt.indexOf('15120688720') >= 0); // this can not be tested as time modified of the file can not be constant always 
     assert.ok(fileCnt.indexOf('ce132772dca7b5f76ec00ab4d7959712') >= 0);
