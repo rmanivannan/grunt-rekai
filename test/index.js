708fb800ea0bfd7b71b7b8852abc058b1bfc0c80
Manivannan R <manivannan13tnj@gmail.com>
2015-11-19 23:54:42 +0530

    Update index.js

--- test/index.js
+++ test/index.js
@@ -8,6 +8,9 @@ describe('grunt-rekai', function() {
     assert.ok(fs.existsSync(filename) && fs.statSync(filename).size, 'Fingerprint file created.');
   });
   it('Should contain correct fingerprint value.', function() {
-    assert.equal(fs.readFileSync(filename,'utf8'), 'var fp1 = "Ao43b9f1RlduffmZRujo7g=="; \nvar fp2 = "15120688720"; \nvar fp3 = "ce132772dca7b5f76ec00ab4d7959712"; ');
+    var fileCnt = fs.readFileSync(filename,'utf8');
+    assert.ok(fileCnt.indexOf('Ao43b9f1RlduffmZRujo7g==') >= 0);
+    assert.ok(fileCnt.indexOf('15120688720') >= 0);
+    assert.ok(fileCnt.indexOf('ce132772dca7b5f76ec00ab4d7959712') >= 0);
   });
 });
